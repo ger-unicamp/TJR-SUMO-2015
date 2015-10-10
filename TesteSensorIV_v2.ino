9bd510bf374930416d4438cf2a3cec4db3f67a9f
thaleslr <thaleslr8@gmail.com>
2015-10-10 00:22:25 -0300

    Update TesteSensorIV_v2.ino

--- TesteSensorIV_v2.ino
+++ TesteSensorIV_v2.ino
@@ -6,13 +6,10 @@ int limiar;
 
 QTRSensorsRC esquerda((unsigned char[]) {14,15}, 2);
 
-//QTRSensorsRC direita((unsigned char[]) {16,17}, 2);
-//QTRSensorsRC frente((unsigned char[]) {18,19}, 2);
-
 void setup(){
   Serial.begin(9600);
   int min, max, times;
-  times = 150;
+  times = 150; //numero de vezes que chamara a funcao de calibracao
   Serial.println("Calibrando:");
   for (int i = 0; i < times; i++){
     if(i%7 == 0){
@@ -21,41 +18,42 @@ void setup(){
       Serial.println(" %)");
     }
     esquerda.calibrate();
-    delay(20);
+    delay(20); //intervalo de tempo entre calibracoes
   }
-  Serial.print("(100 %)");
-  Serial.println();
-  max = *esquerda.calibratedMaximumOn;
-  min = *esquerda.calibratedMinimumOn;
-  Serial.println("maximo: ");
+  Serial.println("(100 %)");
+  
+  max = *esquerda.calibratedMaximumOn; //guardando valores maximo
+  min = *esquerda.calibratedMinimumOn; //e minimo das leituras
+  
+  Serial.println("Máximo: ");
   Serial.println(max);
-  //Serial.println(*esquerda.calibratedMaximumOff);
-
-
-  Serial.println("minimo: ");
+  Serial.println("Mínimo: ");
   Serial.println(min);
-  //Serial.println(*esquerda.calibratedMinimumOff);
   
   int dif = max - min;
-  limiar = dif * 0.5 + min;
+  limiar = dif * 0.5 + min; //definicao do limiar entre preto e branco
 }
 void loop(){
   unsigned int readings[2];
   esquerda.read(readings);
-  //esquerda.emittersOn();
-  Serial.print ("sensor1  ");
-  if(readings[0] >= limiar)
+  
+  Serial.print ("Sensor 1: ");
+  if(readings[0] >= limiar){
     Serial.print("PRETO (");
-  else 
+  }
+  else{
     Serial.print("BRANCO (");
+  }
   Serial.print(readings[0]);
   Serial.println(")");  
   
-  Serial.print ("sensor2  ");
-  if(readings[1] >= limiar)
+  Serial.print ("Sensor 2: ");
+  if(readings[1] >= limiar){
     Serial.print("PRETO (");
-  else 
+  }
+  else{
     Serial.print("BRANCO ("); 
+  }
   Serial.print(readings[1]);
   Serial.println(")"); 
   
